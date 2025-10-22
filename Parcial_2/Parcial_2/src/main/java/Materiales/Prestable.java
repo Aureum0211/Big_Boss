e29288a3ecf30c0d73de64c69ba337dce8aaf933
Aureum0211 <edwardcarrillo@unicesar.edu.co>
2025-10-21 22:28:58 -0500

    Add files via upload

---
+++ Parcial_2/Parcial_2/src/main/java/Materiales/Prestable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Materiales;
+
+/**
+ *
+ * @author lechu
+ */
+public interface Prestable {
+    boolean estaDisponible();
+    void prestar();
+    void devolver();
+}
---
+++ Parcial_2/Parcial_2/src/main/java/Materiales/RevistaCientifica.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Materiales;
+
+/**
+ *
+ * @author lechu
+ */
+public class RevistaCientifica extends MaterialBibliografico {
+    private double factorImpacto;
+
+    public RevistaCientifica(String titulo, String autorNombre, String autorApellido,
+                             int anioPublicacion, String codigoISBN, double factorImpacto) {
+        super(titulo, autorNombre, autorApellido, anioPublicacion, codigoISBN);
+        this.factorImpacto = factorImpacto;
+    }
+
+    @Override
+    public double calcularCostoPrestamo() {
+        return 1000 * factorImpacto;
+    }
+
+    @Override
+    public String generarCitacion() {
+        return getAutorApellido() + ", " + getAutorNombre().charAt(0) + ". (" + getAnioPublicacion() + "). " +
+               getTitulo() + " (Revista Cientifica). FI: " + factorImpacto +
+               ". $" + calcularCostoPrestamo() + ". Estado: " + estadoDisponible();
+    }
+}
